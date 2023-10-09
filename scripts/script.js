512612a6df0888a592ccc4ef6adb425b42d9d399
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-10-09 11:53:59 +0200

    Correción de errores

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
+// https://github.com/jaimecamocha/verificarContrasena.git
+
 function verificarContraseña() {
     var contra = document.getElementById("password").value;
-    
+
+    var sinEspacios = /^\S*$/; // Expresión regular que no permite espacios en blanco
     var longMin = /(?=.{8,})/;
     var caractEspec = /[!@#~]/;
     var mayus = /[A-Z]/;
 
-    if (longMin.test(contra) &&
-        caractEspec.test(contra) &&
-        mayus.test(contra)) {
+    if (sinEspacios.test(contra) && longMin.test(contra) && caractEspec.test(contra) && mayus.test(contra)) {
         document.getElementById("mensaje").innerHTML = "Contraseña aceptada";
     } else {
-        document.getElementById("mensaje").innerHTML = "Contraseña denegada. La contraseña debe tener al menos 8 caracteres, contener '@', '#' o '~', y al menos una letra mayúscula.";
+        document.getElementById("mensaje").innerHTML = "Contraseña denegada. La contraseña debe tener al menos 8 caracteres, no contener espacios en blanco, contener '@', '#' o '~', y al menos una letra mayúscula.";
     }
 }
